ac95fe941f4365ae9e0814d084527f6a6c111a9a
podvistorcheto <denev.s.deyan@gmail.com>
2020-05-07 16:35:03 +0000

    accepting user input

--- table.py
+++ table.py
@@ -1,4 +1,5 @@
 import dash
+from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
 
@@ -6,17 +7,19 @@ app = dash.Dash()
 
 app.layout = html.Div(children=[
     html.H1('Dash_lessons'),
-    dcc.Graph(id='example',
-            figure={
-                'data': [
-                    {'x':[1,2,3,4,5], 'y':[5,6,7,2,1], 'type': 'line', 'name':'boats'},
-                    {'x':[1,2,3,4,5], 'y':[8,3,2,3,5], 'type': 'bar', 'name':'cars'},
-                    ],
-                'layout': {
-                    'title': 'Basic Dash Example'
-                }
-                })
+    dcc.Input(id='input', value='Enter something', type='text'),
+    html.Div(id='output')
     ])
+
+@app.callback(
+    Output(component_id='output', component_property='children'),
+    [Input(component_id='input', component_property='value')])
+def update_value(input_data):
+    """input random number will bring it automatically to the power of 2"""
+    try:
+        return str(float(input_data)**2)
+    except:
+        return "Some error"
 
 if __name__ == "__main__":
     app.run_server(debug=True)
